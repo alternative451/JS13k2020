50c24dc1f1dd4408f556fb8c4a8cf13fd427a942
alternative451 <alternative451@myopera.com>
2020-09-05 10:16:54 +0200

    add missing wokin files

---
+++ src/assets/maps/aos.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="aos" tilewidth="1" tileheight="1" tilecount="480000" columns="800">
+ <image source="../Pictures/aos.png" width="800" height="600"/>
+</tileset>
